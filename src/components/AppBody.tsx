311cdf7fcdb655a9793d8f8c4bee6110457be657
Sebastian Herrmann <sebastian@herrherrmann.net>
2019-12-01 17:30:20 +0100

    Use proper HTML element for main part of page

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const headerHeight = '55px';
 
-const Body = styled.header(({ theme }) => ({
+const Body = styled.main(({ theme }) => ({
 	maxWidth: theme.widths.maxContentWidth,
 	minHeight: `calc(100vh - ${headerHeight})`,
 	margin: '0 auto',
