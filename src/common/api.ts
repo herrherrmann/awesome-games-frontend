74480f3ffe99f69836d2053b84e21670a3991971
Sebastian Herrmann <sebastian@herrherrmann.net>
2020-05-02 01:55:29 +0200

    Add hint about local api server

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 
+/**
+ * Backend API, use 'http://localhost:3000' for local development.
+ */
 const API = 'https://awesome-games-api.herokuapp.com';
 
 const api = Axios.create({
