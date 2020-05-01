77c64b89d12ff7465e093ea18837d6fefdc51a76
Sebastian Herrmann <sebastian@herrherrmann.net>
2020-05-01 13:16:16 +0200

    Remove unneeded hidden overflow, thus fixing focus style

--- src/components/Games/Filters/FilterLabelWrapper.tsx
+++ src/components/Games/Filters/FilterLabelWrapper.tsx
@@ -3,8 +3,6 @@ import styled from '../../../common/theme';
 const FilterLabelWrapper = styled.div(({ theme }) => ({
 	marginTop: 0,
 	marginBottom: theme.spacings.default,
-	overflow: 'hidden',
-	textOverflow: 'ellipsis',
 	whiteSpace: 'nowrap',
 }));
 
