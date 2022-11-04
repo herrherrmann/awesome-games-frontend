b2383172400d06a038bf6c74778ad4ed4fa748f6
Sebastian Herrmann <sebastian@herrherrmann.net>
2022-11-04 10:17:40 +0100

    Change API address to use deployment on Render

--- src/common/api.ts
+++ src/common/api.ts
@@ -3,7 +3,7 @@ import Axios from 'axios';
 /**
  * Backend API, use 'http://localhost:3000' for local development.
  */
-const API = 'https://awesome-games-api.herokuapp.com';
+const API = 'https://awesome-games-api.onrender.com';
 
 const api = Axios.create({
 	baseURL: API,
